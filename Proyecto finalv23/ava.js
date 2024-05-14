fc98dae7a127a0d141e82d1b1367d539060a0c5f
mariferflores <160251657+mariferflores@users.noreply.github.com>
2024-05-14 08:28:33 -0600

    Add files via upload

---
+++ Proyecto finalv23/ava.js
@@ -0,0 +1,32 @@
+function mostrarVideos() {
+    ocultarTodo();
+    document.getElementById('video-gallery').classList.add('active');
+}
+
+function mostrarFotos() {
+    ocultarTodo();
+    document.getElementById('photo-gallery').classList.add('active');
+}
+
+function mostrarFAQ() {
+    ocultarTodo();
+    document.getElementById('faq').classList.add('active');
+}
+
+function ocultarTodo() {
+    var galleries = document.querySelectorAll('.gallery');
+    galleries.forEach(function(gallery) {
+        gallery.classList.remove('active');
+    });
+}
+
+document.addEventListener('click', function(event) {
+    var menuToggle = document.querySelector('.menu-toggle');
+    var menu = document.querySelector('.menu');
+
+    // Si el clic ocurre fuera del menú y el menú está abierto, cierra el menú
+    if (!menuToggle.contains(event.target) && !menu.contains(event.target) && menuToggle.querySelector('input').checked) {
+        menuToggle.querySelector('input').checked = false;
+    }
+});
+
